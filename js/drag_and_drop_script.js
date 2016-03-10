ba0eb22d0cab9422f3abf3b9afca65d7623f80b3
TWasilonek <tomasz.wasilonek@gmail.com>
2016-03-10 13:14:26 +0100

    drag and drop viusal feedback added

--- js/drag_and_drop_script.js
+++ js/drag_and_drop_script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
   var totalOperator = $('.operator');
   var totalCleared = $('.cleared');
 
-   //Draggable elements functionalities
+  //Draggable elements functionalities
     draggablesParent.on('dragstart', '.draggable', function(e){
       // Take the text from the current element
       var value = $(this).text();
@@ -26,4 +26,29 @@ $(document).ready(function(){
     draggablesParent.on('dragend', '.draggable', function(e) {
       dropzones.removeClass('dropzone-revealed');
     });
+
+  //Dropzones functionalities
+    dropzones.on('dragenter', function(e){
+      e.preventDefault();
+      $(this).addClass('dropzone-hover');
+    });
+
+    dropzones.on('dragleave', function(e){
+      e.preventDefault();
+      $(this).removeClass('dropzone-hover');
+    });
+
+    //cancel the deaful action of the last events dragenter and dragover - this is NEEDED in order to ALLOW THE DROP EVENT
+    dropzones.on('dragenter', function(e){ e.preventDefault(); }, false);
+    dropzones.on('dragover', function(e){ e.preventDefault(); }, false);
+
+    //call the drop event
+    dropzones.on('drop', function(e){
+      console.log('dropped');
+      e.preventDefault();
+      e.dataTransfer = e.originalEvent.dataTransfer;
+      $(this).text(e.dataTransfer.getData('text/plain'));
+      $(this).removeClass('dropzone-hover');
+    });
+
 });
